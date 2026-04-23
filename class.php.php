7edda6515aedb84101d6856fb695ac1b810dfd44
Lorenzo Urbano <lorenzourbano92@gmail.com>
2026-04-23 13:43:44 +0200

    completato

---
+++ class.php.php
@@ -0,0 +1,41 @@
+<?php
+
+abstract class Category{
+    abstract public function getMyCategory();
+}
+
+class Attualita extends Category{
+    public function getMyCategory() {
+        echo "La categoria è Attualità\n";
+    }
+}
+
+class Sport extends Category{
+    public function getMyCategory() {
+        echo "La categoria è Sport\n";
+    }
+}
+
+class Gossip extends Category{
+    public function getMyCategory() {
+        echo "La categoria è Gossip\n";
+    }
+}
+
+class Storia extends Category{
+    public function getMyCategory() {
+        echo "La categoria è Storia\n";
+    }
+}
+
+
+// ISTANZA
+$attualita = new Attualita();
+$attualita->getMyCategory();
+
+
+
+
+
+
+?>
